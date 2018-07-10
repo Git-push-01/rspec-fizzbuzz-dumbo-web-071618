485d1a4d8dc02fbc63a07f91a90ce2599623981e
Hugo Escandon <hescandon6@gmail.com>
2018-07-10 15:23:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
 def fizzbuzz(i)
-  if i % 3 == 0
+
+if i % 3 == 0 && i % 5 == 0
+    return "FizzBuzz"
+  elsif  i % 3 == 0
     return "Fizz"
   elsif i % 5 == 0
       return "Buzz"
     elsif i % 3 == 0 && i % 5 == 0
         return "FizzBuzz"
-      else i % 3 != 0 && i % 5 != 0
+      else
         return nil
 
 
